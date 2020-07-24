e76af7302ec3267846bb8ed0e9f3316d17e2e3e2
Jack Wong <jackwong@Jacks-MBP.fios-router.home>
2020-07-24 16:19:10 -0400

    Added custom tarnsition to fullscreen

---
+++ SwiftUI 2.0/DesignCode/DesignCode/DesignCode/View/CourseList.swift
@@ -0,0 +1,58 @@
+//
+//  CourseList.swift
+//  DesignCode
+//
+//  Created by Jack Wong on 7/24/20.
+//
+
+import SwiftUI
+
+struct CourseList: View {
+    var body: some View {
+        VStack {
+            CourseView()
+        }
+    }
+}
+
+struct CourseList_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseList()
+            .previewDevice("iPhone 11 Pro")
+    }
+}
+
+struct CourseView: View {
+    @State var show = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("SwiftUI Advanced")
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundColor(.white)
+                Text("20 Sections")
+                    .foregroundColor(Color.white.opacity(0.7))
+                Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+            }
+            Spacer()
+            Image(uiImage: #imageLiteral(resourceName: "Card2"))
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
+                .frame(height: 140, alignment: .top)
+        }
+        .padding(show ? 30 : 20)
+        .padding(.top, show ? 30 : 20)
+//        .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
+        .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280)
+        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)).opacity(0.3), radius: 20, x: 0, y: 20)
+        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        .onTapGesture {
+            show.toggle()
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
