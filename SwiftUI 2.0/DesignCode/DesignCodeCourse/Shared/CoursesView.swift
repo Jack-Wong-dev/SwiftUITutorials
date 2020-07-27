678b1ce412c909f2c725929b23beb0791959efc4
Jack Wong <jackwong@Jacks-MBP.fios-router.home>
2020-07-27 17:31:54 -0400

    matched container and zIndex

--- SwiftUI 2.0/DesignCode/DesignCodeCourse/Shared/CoursesView.swift
+++ SwiftUI 2.0/DesignCode/DesignCodeCourse/Shared/CoursesView.swift
@@ -17,38 +17,71 @@ struct CoursesView: View {
         //Card to Full Screen ScrollView (with list) and vice versa
         ZStack {
             ScrollView {
-//            /*Fits as many 160 size items with gap value (spacing: 16 between columns), with 16 spacing between rows */
+            /*Fits as many 160 size items with gap value (spacing: 16 between columns), with 16 spacing between rows */
                 LazyVGrid(
                     columns: [ GridItem(.adaptive(minimum: 160),spacing: 16)],
                     spacing: 16
                 ) {
                     //Cards
                     ForEach(courses) { item in
-                        CourseItem(course: item) //Matched Geometry Effect always before frame. isSource is optional, helps determine where to animation from.  e.g. Card to Other Card, Other Card to Card
-                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                            .frame(height: 200)
-                            .onTapGesture {
-                                /* withAnimation is preferred over .animation when Matched Geoemetry Effect is involved.
-                                 .animation creates lag (may change in the future), here the card behind would try to play catch up instead of being directly behind */
-                                withAnimation(.spring()) {
-                                    show.toggle()
-                                    selectedItem = item
-                                    isDisabled = true
+                        VStack {
+                            CourseItem(course: item) //Matched Geometry Effect always before frame. isSource is optional, helps determine where to animation from.  e.g. Card to Other Card, Other Card to Card
+                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                                .frame(height: 200)
+                                .onTapGesture {
+                                    /* withAnimation is preferred over .animation when Matched Geoemetry Effect is involved.
+                                     .animation creates lag (may change in the future), here the card behind would try to play catch up instead of being directly behind */
+                                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
+                                        show.toggle()
+                                        selectedItem = item
+                                        isDisabled = true
+                                    }
                                 }
-                            }
-                            .disabled(isDisabled) //Tap anymore after tapping the card
+                                .disabled(isDisabled)  //Disable card
+                        }
+                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
                     }
                 }
                 .padding(16)
                 .frame(maxWidth: .infinity)
             }
+            .zIndex(1)
             
             if selectedItem != nil {
                 //Full Screen
-                ScrollView {
-                    CourseItem(course: selectedItem!)
-                        .matchedGeometryEffect(id: selectedItem!.id, in: namespace) //always must be before frame
-                        .frame(height: 300)
+                ZStack(alignment: .topTrailing) {
+                    VStack {
+                        ScrollView {
+                            CourseItem(course: selectedItem!)
+                                .matchedGeometryEffect(id: selectedItem!.id, in: namespace) //always must be before frame
+                                .frame(height: 300)
+                            
+                            //Content
+                            VStack {
+                                ForEach(0 ..< 20) { item in
+                                    CourseRow()
+                                }
+                            }
+                            .padding()
+                        }
+                    }
+                    .background(Color("Background 1"))
+                    .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+                    .matchedGeometryEffect(id: "container\(selectedItem!.id)", in: namespace)
+                    //                .transition(
+                    //                    /* Content shows up 0.3 seconds after the transition from card to scrollview.  .asymmetric is required so that the delay doesn't apply when going from scrollview to card */
+                    //                    .asymmetric(
+                    //                        insertion: AnyTransition
+                    //                            .opacity
+                    //                            .animation(Animation.spring().delay(0.3)),
+                    //                        removal: AnyTransition
+                    //                            .opacity
+                    //                            .animation(.spring()))
+                    //                )
+                    .edgesIgnoringSafeArea(.all)
+                    
+                    CloseButton()
+                        .padding(.trailing, 16)
                         .onTapGesture {
                             withAnimation(.spring()) {
                                 show.toggle()
@@ -61,28 +94,8 @@ struct CoursesView: View {
                                 }
                             }
                         }
-                    
-                    //Content
-                    VStack {
-                        ForEach(0 ..< 20) { item in
-                            CourseRow()
-                        }
-                    }
-                    .padding()
                 }
-                .background(Color("Background 1"))
-                .transition(
-                    .asymmetric(
-                        insertion: AnyTransition
-                            .opacity
-                            .animation(Animation.spring().delay(0.3)),
-                        removal: AnyTransition
-                            .opacity
-                            .animation(.spring()))
-                    
-                    /* Content shows up 0.3 seconds after the transition from card to scrollview.  .asymmetric is required so that the delay doesn't apply when going from scrollview to card */
-                )
-                .edgesIgnoringSafeArea(.all)
+                .zIndex(2)
             }
         }
     }
