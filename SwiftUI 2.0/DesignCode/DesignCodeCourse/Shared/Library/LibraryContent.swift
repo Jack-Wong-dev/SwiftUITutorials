fa9649da49a1690b75d11af8f12ef1d756c67db9
Jack Wong <jackwong@Jacks-MBP.fios-router.home>
2020-07-28 11:54:43 -0400

    Library Content and Modifiers

---
+++ SwiftUI 2.0/DesignCode/DesignCodeCourse/Shared/Library/LibraryContent.swift
@@ -0,0 +1,37 @@
+//
+//  LibraryContent.swift
+//  DesignCodeCourse
+//
+//  Created by Jack Wong on 7/28/20.
+//
+
+import SwiftUI
+
+struct LibraryContent: LibraryContentProvider {
+    @LibraryContentBuilder
+    var views: [LibraryItem] {
+        LibraryItem(
+            CloseButton(),
+            title: "Close Button View",
+            category: .control
+        )
+    }
+    
+    @LibraryContentBuilder
+    func modifiers(base: Image) -> [LibraryItem] {
+        LibraryItem(
+            base.cardStyle(color: Color.blue, cornerRadius: 22),
+            title: "Card Style",
+            category: .effect
+        )
+    }
+}
+
+extension View {
+    func cardStyle(color: Color, cornerRadius: CGFloat) -> some View {
+        return self
+            .background(color)
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
+            .shadow(color: Color.blue.opacity(0.3), radius: 20, x: 20, y: 10)
+    }
+}
