8e135d0260dbe5d2a96b8c282100e46c16f26daf
Jack Wong <jackwong@Jacks-MBP.fios-router.home>
2020-08-24 19:32:13 -0400

    Added Detail View

---
+++ SwiftUI 2.0/Fructus/Fructus/App/FruitDetailView.swift
@@ -0,0 +1,69 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Jack Wong on 8/24/20.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    //MARK: - Properties
+    var fruit: Fruit
+    
+    
+    //MARK: - Body
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20) {
+                    // Header
+                    
+                    FruitHeaderView(fruit: fruit)
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        // Title
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // Headline
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        // Nutrients
+                        
+                        // SubHeadline
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // Description
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        // Link
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                    }
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }
+            }
+            .navigationBarTitle(fruit.title, displayMode: .inline)
+            .navigationBarHidden(true)
+            .edgesIgnoringSafeArea(.top)
+        } //: Navigation
+    }
+}
+
+//MARK: - Preview
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[0])
+            .previewDevice("iPhone 11 Pro")
+    }
+}
